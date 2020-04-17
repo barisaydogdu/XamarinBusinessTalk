dbb8c1ad99577ae99a62d3086c2b23d7e973e707
barisaydogdu <46091973+barisaydogdu@users.noreply.github.com>
2020-04-17 16:41:09 +0300

    BusinessTalkw

---
+++ WebService/AAkademiSignalR2/AAkademiSignalR2/ChatHub.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+
+namespace AAkademiSignalR2
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string username, string message)
+        {
+            Clients.All.MessageReceived(username, message);
+        }
+      
+        public override Task OnConnected()
+        {
+            Clients.All.MessageReceived(Context.QueryString["username"]," Bağlandı!");
+            ConnectedUser.Ids.Add(Context.ConnectionId);
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            Clients.All.MessageReceived(Context.QueryString["username"]
+                , " Bağlantı Koptu!");
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            Clients.All.MessageReceived(Context.QueryString["username"], " Yeniden Bağlandı!");
+            return base.OnReconnected();
+        }
+
+        public  void SendMessageToGroup(string username,string message,string groupname)
+        {
+            Clients.Group(groupname).MessageReceived(username, message);
+        }
+        public Task AddGroups(string groupname)
+        {  
+            return Groups.Add(Context.ConnectionId,groupname);
+        }
+        public void denemegroupchat(string groupname,string username,string message)
+        {
+            Clients.Group(groupname, Context.ConnectionId, Context.ConnectionId).addChatMessage(username, message);
+        }
+        public void AddList(string room)
+        {
+            // Create a list  
+            List<string> AuthorList = new List<string>();
+            // Add items using Add method   
+            AuthorList.Add(room);
+        }
+        public async Task RemoveFromGroup(string username, string groupName)
+        {
+            await Clients.Group(groupName).SendAsync("Send", $"{username} is leaving the group {groupName}.");
+            await Groups.Remove(Context.ConnectionId,"Baris" );
+        }
+    }
+}
---
+++ WebService/AAkademiSignalR2/AAkademiSignalR2/Scripts/chat.js
@@ -0,0 +1,52 @@
+﻿//var _connection = new signalR.HubConnectionBuilder()
+document.getElementById("join-group").addEventListener("click", async (event) => {
+    var _userName = document.getElementById(userName).value;
+    var groupName = document.getElementById(roomInput).value;
+    try {
+        await _connection.invoke("AddToGroup", _userName, groupName);
+    }
+    catch (e) {
+        console.error(e.toString());
+    }
+    event.preventDefault();
+});
+//////////////////////////////AKADEMİ Kodları
+//document.getElementById("join-group").addEventListener("click", async (event) => {
+//    try {
+//        await connection.invoke("addToGroup", chatInput.val, roomInput.val());
+//    }
+//    catch (e) {
+//        console.error(e.toString());
+//    }
+//    event.preventDefault();
+//    });
+$(document).ready(function () {
+    var chatInput = $("#txtInput");
+    var roomInput = $("#txtRoom");
+    var userName = prompt("Enter a username");
+    var chat = $.connection.chatHub;
+    //    addToGroup(chatHub)
+    chat.connection.qs = { 'username': userName };
+    chat.client.messageReceived = function (username, message) {
+        $("ul").append("<li><b>" + username + ": </b><i>"
+            + message + "</i></li>");
+    };
+
+    $.connection.hub.start().done(function () {
+        console.log("hub started!");
+        chatInput.keydown(function (e) {
+            if (e.which === 13) {
+                var text = chatInput.val();
+                chat.server.sendMessage(userName, text);
+                chatInput.val("");
+                self.focus();
+            }
+        })
+    });
+
+    chatInput.focus();
+});
+
+
+
+
